cd25f680a81f2174eeec9e7874edd4d0ae8ba572
desiatcodaniel <desiatcodaniel@gmail.com>
2023-01-29 22:32:47 -0800

    strict mode refresh

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,9 @@
+//use strict to forbid certain things and show more specific errors
+//it will also not allow creation of variables using reserved names as variable names
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+//Will throw an error that the variable is undefined because of strict mode
+//if(passTest) hasDriverLicense = true;
